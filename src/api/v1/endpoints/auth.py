7ead21c342d06d8a114c300fabeaa757b7b6e65e
fufu <roycelang24@gmail.com>
2025-12-27 15:04:39 +0800

    feat: delete cookie when auth fail

--- src/api/v1/endpoints/auth.py
+++ src/api/v1/endpoints/auth.py
@@ -57,13 +57,14 @@ async def sign_in(login_data: UserLogin, response: Response, db: AsyncSession = 
 
 
 @verify_router.get("/")
-async def verify(user_id: str = Depends(get_current_user)):
+async def verify(response: Response, user_id: str = Depends(get_current_user)):
     try:
         # 使用 get_current_user 验证 token
         print(user_id)
         
         # 如果返回的是字典（错误情况），说明 token 无效
         if isinstance(user_id, dict):
+            response.delete_cookie(key="access_token")
             return APIResponse(**user_id)
 
         # Token 有效，返回成功
@@ -71,4 +72,5 @@ async def verify(user_id: str = Depends(get_current_user)):
     
     except Exception as e:
         # 处理其他异常
+        response.delete_cookie(key="access_token")
         return APIResponse(retcode=401, message=str(e), data=None)
